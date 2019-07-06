d68851ed237da5e673137c61dcc866d5a2a975b1
Brennen <bspiers90@comcast.net>
2019-07-05 20:30:26 -0400

    Added functions enterProblem, validateSolution, printGridMatrix, and runTest. Terminal options to pick features. Added comments.

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,98 +1,11 @@
-#Easy Puzzle 4,682,668,579 from https://www.websudoku.com/?level=1&set_id=4682668579
-# second puzzle https://www.websudoku.com/?level=1&set_id=3948377814
+#solves sudoku puzzles
+#written by bpiers
+#improve by detecting unsolvable sudoku puzzles
 size = 3
 gridSize = size * size
-grid =    [[7,1,0,9,0,0,0,0,8],
-           [0,0,0,0,0,6,9,0,0],
-           [9,6,0,4,1,0,0,0,0],
-           [0,0,0,0,9,2,8,6,3],
-           [3,0,9,0,6,0,2,0,1],
-           [2,8,6,3,5,0,0,0,0],
-           [0,0,0,0,8,3,0,4,9],
-           [0,0,3,5,0,0,0,0,0],
-           [1,0,0,0,0,9,0,3,2]]
 
-gridOG =  [[7,1,0,9,0,0,0,0,8],
-           [0,0,0,0,0,6,9,0,0],
-           [9,6,0,4,1,0,0,0,0],
-           [0,0,0,0,9,2,8,6,3],
-           [3,0,9,0,6,0,2,0,1],
-           [2,8,6,3,5,0,0,0,0],
-           [0,0,0,0,8,3,0,4,9],
-           [0,0,3,5,0,0,0,0,0],
-           [1,0,0,0,0,9,0,3,2]]
-
-solution =[[7,1,4,9,3,5,6,2,8],
-           [8,3,5,2,7,6,9,1,4],
-           [9,6,2,4,1,8,3,7,5],
-           [5,4,1,7,9,2,8,6,3],
-           [3,7,9,8,6,4,2,5,1],
-           [2,8,6,3,5,1,4,9,7],
-           [6,2,7,1,8,3,5,4,9],
-           [4,9,3,5,2,7,1,8,6],
-           [1,5,8,6,4,9,7,3,2]]
-
-gridOG2 = [[0,0,0,7,0,0,0,0,8],
-           [4,0,0,0,0,0,0,3,0],
-           [0,8,0,5,3,0,4,0,7],
-           [0,6,0,0,0,7,0,0,0],
-           [8,0,2,0,0,0,5,0,6],
-           [0,0,0,9,0,0,0,1,0],
-           [1,0,8,0,6,4,0,7,0],
-           [0,4,0,0,0,0,0,0,3],
-           [7,0,0,0,0,5,0,0,0]]
-
-gridOG3 = [[0,2,0,0,3,0,2,0,0],
-           [0,8,0,4,0,0,0,0,0],
-           [0,2,0,0,0,5,0,6,1],
-           [0,9,0,1,0,0,0,7,0],
-           [2,0,0,0,4,0,0,0,9],
-           [0,3,0,0,0,9,0,8,0],
-           [3,1,0,7,0,0,0,4,0],
-           [0,0,0,0,0,4,0,5,0],
-           [0,0,6,0,9,0,0,2,0]]
-
-gridOG4 = [[0,1,6,2,3,0,0,0,9],
-           [0,8,0,0,0,0,0,0,0],
-           [0,0,0,1,0,7,0,0,5],
-           [0,0,0,0,0,4,0,9,8],
-           [0,0,1,0,0,0,3,0,0],
-           [8,2,0,6,0,0,0,0,0],
-           [6,0,0,4,0,1,0,0,0],
-           [0,0,0,0,0,0,0,7,0],
-           [3,0,0,0,6,8,4,2,0]]
-
-hardest = [[8,0,0,0,0,0,0,0,0],
-           [0,0,3,6,0,0,0,0,0],
-           [0,7,0,0,9,0,2,0,0],
-           [0,5,0,0,0,7,0,0,0],
-           [0,0,0,0,4,5,7,0,0],
-           [0,0,0,1,0,0,0,3,0],
-           [0,0,1,0,0,0,0,6,8],
-           [0,0,8,5,0,0,0,1,0],
-           [0,9,0,0,0,0,4,0,0]]
-
-gridOG5 = [[0,2,8,1,0,0,0,0,0],
-           [4,0,0,0,8,0,0,3,0],
-           [0,0,0,0,2,0,0,0,0],
-           [7,3,0,0,0,0,0,0,6],
-           [0,0,4,0,0,0,8,5,0],
-           [0,0,5,0,0,9,0,0,0],
-           [0,0,2,0,3,6,0,0,9],
-           [8,0,0,9,0,0,0,0,7],
-           [0,6,0,2,0,0,0,4,0]]
-
-gridOG6 = [[7,9,0,0,0,0,0,5,0],
-           [4,0,0,6,0,0,0,0,0],
-           [0,0,0,7,4,2,0,6,0],
-           [0,7,9,5,0,0,0,4,8],
-           [0,0,0,0,0,0,0,0,0],
-           [8,3,0,0,0,4,9,1,0],
-           [0,4,0,1,2,9,0,0,0],
-           [0,0,0,0,0,6,0,0,7],
-           [0,6,0,0,0,0,0,9,3]]
-
-def makeGrid(gridSize) : #creates a blank sudoku grid
+#creates a sudoku grid of 0s
+def makeGrid(gridSize) :
     gridRow=[]
     grid0=[]
     for i in range(0, gridSize**2) :
@@ -102,6 +15,7 @@ def makeGrid(gridSize) : #creates a blank sudoku grid
             gridRow = []
     return grid0
 
+#creates copy of sudoku grid
 def copyGrid(originalGrid) :
     newGrid = makeGrid(gridSize)
     for i in range(0, gridSize) :
@@ -109,21 +23,20 @@ def copyGrid(originalGrid) :
             newGrid[i][j] = originalGrid[i][j]
     return newGrid
 
-
+#checks if newNumber is unique in set of compareNumbers
 def uniqueCheck(newNumber, compareNumbers) :
-    #number is what the user inputed.
-    #cell is the [row,column] of the number
     for currentNumber in compareNumbers :
         if newNumber == currentNumber :
             return False
     return True
 
-
+#groups numbers from a Row into an array
 def Row(i, currentGrid) :
     row = currentGrid[i][:]
     #print(row)
     return row
 
+#groups numbers from a Column into an array
 def Column(j, currentGrid) :
     column = []
     for n in range(0, gridSize) :
@@ -131,6 +44,7 @@ def Column(j, currentGrid) :
     #print(column)
     return column
 
+#groups numbers from a Box into an array
 def Box(i,j, currentGrid) :
     boxRowLoc = int(i/(gridSize**(0.5)))*size
     #print(boxRowLoc)
@@ -143,6 +57,8 @@ def Box(i,j, currentGrid) :
     #print(box)
     return box
 
+#checks newNumber against Row, Column, and Box for uniqueness
+#ensures newNumber abides by sudoku rules
 def checkRowColumnBox(newNumber, currentGrid, i, j) :
     result = []
     checkThese = [Row(i, currentGrid), Column(j, currentGrid), Box(i,j, currentGrid)]
@@ -152,8 +68,9 @@ def checkRowColumnBox(newNumber, currentGrid, i, j) :
             return False
     return True
 
-#grid = makeGrid(gridSize)
-def printGrid(inputGrid) :
+#prints sudoku grid as a matrix
+#old function to print sudoku grid
+def printGridMatrix(inputGrid) :
     row = []
     for i in range(0, len(inputGrid[0])) :
         for j in range(0, len(inputGrid[0])) :
@@ -163,16 +80,28 @@ def printGrid(inputGrid) :
                 row = []
     return
 
-
-
-def solver2(guesses) :
-    for i in range(0, gridSize) :
-        for j in range(0, gridSize) :
-            #print('i'+str(i)+' j'+str(j))
-            if guesses[i][j] != 0 :
-                return
+#prints sudoku grid as stylized grid
+def printGrid(inputGrid) :
+    rowLength = size ** 2
+    i1 = 0
+    i2 = 0
+    for i in range(0, rowLength) :
+        for j in range(0, rowLength) :
+            if inputGrid[i][j] == 0 :
+                print('_', end='  ')
+            else :
+                print(inputGrid[i][j], end='  ')
+            if i1%rowLength != rowLength - 1 :
+                if i1%size == size - 1 :
+                    print('|', end='  ')
+            else :
+                print('')
+                if i1%(rowLength*3) == rowLength*3 - 1 and i1 < rowLength**2 - 1:
+                    print('---------+-----------+---------')
+            i1 = i1 + 1
     return
 
+#moves the focues of solver function to previous cell
 def moveBack(problem, x, y) :
     i = x
     j = y
@@ -185,18 +114,17 @@ def moveBack(problem, x, y) :
                 j = j - 1
             if problem[i][j] == 0 :
                 if i<0 or j<0 :
-                    print('failed to solve')
-                    return False
+                    print('Failed to solve.')
+                    return gridSize, gridSize
                 return i, j
             if j == 0 :
                 i = i - 1
                 j = gridSize
-    print('i have failed to solve your sudoku. *performs seppuku*')
-    return False
+    print('Failed to solve.')
+    return gridSize, gridSize
 
-def moveForward(problem, x, y) : #still need to fix this
-    i = x
-    j = y
+#moves the focus of solver function to next cell
+def moveForward(problem, i, j) : #still need to fix this
     j = j + 1
     if j >= gridSize :
         i = i + 1
@@ -213,20 +141,12 @@ def moveForward(problem, x, y) : #still need to fix this
                 print('sudoku solved')
                 return i,j
     return i, j
-    # while i < gridSize and j < gridSize :
-    #     if j >= gridSize :
-    #         i = i + 1
-    #         j = 0
-    #     while j <= gridSize :
-    #         if problem[i][j] == 0 :
-    #             return i, j
-    #         if j == gridSize :
-    #             i = i + 1
-    #             j = 0
-    # print('i have solved your sudoku.')
-    # return False
 
-def solver3(problem) :
+#solves the sudoku puzzle
+def solver(problem) :
+    if not validateSolution(problem) :
+        return False
+    print('attempting to solve sudoku puzzle...')
     killSwitch = 0
     progress = copyGrid(problem)
     i = 0
@@ -242,10 +162,10 @@ def solver3(problem) :
             if killSwitch > 1000000 :
                 print('timed out. killSwitch activated')
                 return progress
-            for attempt in range(progress[i][j] + 1, gridSize+1) :
-                attemptResult = checkRowColumnBox(attempt, progress, i, j)
-                if attemptResult == True :
-                    progress[i][j] = attempt
+            for guess in range(progress[i][j] + 1, gridSize+1) :
+                guessResult = checkRowColumnBox(guess, progress, i, j)
+                if guessResult == True :
+                    progress[i][j] = guess
                     #print('write '+str(progress[i][j])+'  i'+str(i)+' j'+str(j))
                     move = moveForward(problem, i, j)
                     i = move[0]
@@ -256,33 +176,175 @@ def solver3(problem) :
                     progress[i][j] = 0
                     #print('forward  i'+str(i)+' j'+str(j))
                     break
-                if attempt == gridSize :
+                if guess == gridSize :
                     progress[i][j] = 0
                     move = moveBack(problem, i, j)
                     i = move[0]
                     j = move[1]
                     #print('back     i'+str(i)+' j'+str(j)+' '+str(progress[i][j]))
-                    if progress[i][j] == gridSize : #this is an example of bad code
+                    while progress[i][j] == gridSize :
                         progress[i][j] = 0
                         move = moveBack(problem, i, j)
                         i = move[0]
                         j = move[1]
-                        #print('back2    i'+str(i)+' j'+str(j)+' '+str(progress[i][j]))
-                        if progress[i][j] == gridSize : #this is even worse
-                            progress[i][j] = 0
-                            move = moveBack(problem, i, j)
-                            i = move[0]
-                            j = move[1]
-                            #print('back3    i'+str(i)+' j'+str(j)+' '+str(progress[i][j]))
-                            if progress[i][j] == gridSize : #not even trying at this point
-                                progress[i][j] = 0
-                                move = moveBack(problem, i, j)
-                                i = move[0]
-                                j = move[1]
-                                print('back4    i'+str(i)+' j'+str(j)+' '+str(progress[i][j]))
-    #print(killSwitch)
+    validateSolution(progress)
     return progress
 
-printGrid(gridOG6)
-print('')
-printGrid(solver3(gridOG6))
+def validateSolution(solution) :
+    for i in range(0, gridSize) :
+        for j in range(0, gridSize) :
+            temp = solution[i][j]
+            if temp == 0 :
+                continue
+            solution[i][j] = -1
+            flag = checkRowColumnBox(temp, solution, i, j)
+            solution[i][j] = temp
+            if not flag :
+                print('failed validation check. puzzle does not meet sudoku rules.')
+                return flag
+    return flag
+
+#user enters numbers from their sudoku puzzle
+#user choice (1) calls this function
+def enterProblem() :
+    problemGrid = makeGrid(gridSize)
+    for i in range(0, gridSize) :
+        for j in range(0, gridSize) :
+            inputNumber = -1
+            while inputNumber < 0 or inputNumber > gridSize :
+                if inputNumber != -1 :
+                    print('Number not 1-9. ', end='')
+                print('Enter number : ', end='')
+                inputNumber = input()
+                if inputNumber == '' :
+                    inputNumber = 0
+                try :
+                    inputNumber = int(inputNumber)
+                except ValueError :
+                    print('Not a valid number. ', end='')
+                    inputNumber = -1
+            problemGrid[i][j] = inputNumber
+            printGrid(problemGrid)
+    return problemGrid
+
+#test cases to validate changes to program
+#user choice (2) calls this function
+def runTest() :
+    grid =    [[7,1,0,9,0,0,0,0,8],
+               [0,0,0,0,0,6,9,0,0],
+               [9,6,0,4,1,0,0,0,0],
+               [0,0,0,0,9,2,8,6,3],
+               [3,0,9,0,6,0,2,0,1],
+               [2,8,6,3,5,0,0,0,0],
+               [0,0,0,0,8,3,0,4,9],
+               [0,0,3,5,0,0,0,0,0],
+               [1,0,0,0,0,9,0,3,2]]
+
+    gridOG =  [[7,1,0,9,0,0,0,0,8],
+               [0,0,0,0,0,6,9,0,0],
+               [9,6,0,4,1,0,0,0,0],
+               [0,0,0,0,9,2,8,6,3],
+               [3,0,9,0,6,0,2,0,1],
+               [2,8,6,3,5,0,0,0,0],
+               [0,0,0,0,8,3,0,4,9],
+               [0,0,3,5,0,0,0,0,0],
+               [1,0,0,0,0,9,0,3,2]]
+
+    gridOG2 = [[0,0,0,7,0,0,0,0,8],
+               [4,0,0,0,0,0,0,3,0],
+               [0,8,0,5,3,0,4,0,7],
+               [0,6,0,0,0,7,0,0,0],
+               [8,0,2,0,0,0,5,0,6],
+               [0,0,0,9,0,0,0,1,0],
+               [1,0,8,0,6,4,0,7,0],
+               [0,4,0,0,0,0,0,0,3],
+               [7,0,0,0,0,5,0,0,0]]
+
+    gridOG3 = [[0,2,0,0,3,0,2,0,0],
+               [0,8,0,4,0,0,0,0,0],
+               [0,2,0,0,0,5,0,6,1],
+               [0,9,0,1,0,0,0,7,0],
+               [2,0,0,0,4,0,0,0,9],
+               [0,3,0,0,0,9,0,8,0],
+               [3,1,0,7,0,0,0,4,0],
+               [0,0,0,0,0,4,0,5,0],
+               [0,0,6,0,9,0,0,2,0]]
+
+    gridOG4 = [[0,1,6,2,3,0,0,0,9],
+               [0,8,0,0,0,0,0,0,0],
+               [0,0,0,1,0,7,0,0,5],
+               [0,0,0,0,0,4,0,9,8],
+               [0,0,1,0,0,0,3,0,0],
+               [8,2,0,6,0,0,0,0,0],
+               [6,0,0,4,0,1,0,0,0],
+               [0,0,0,0,0,0,0,7,0],
+               [3,0,0,0,6,8,4,2,0]]
+
+    hardest = [[8,0,0,0,0,0,0,0,0],
+               [0,0,3,6,0,0,0,0,0],
+               [0,7,0,0,9,0,2,0,0],
+               [0,5,0,0,0,7,0,0,0],
+               [0,0,0,0,4,5,7,0,0],
+               [0,0,0,1,0,0,0,3,0],
+               [0,0,1,0,0,0,0,6,8],
+               [0,0,8,5,0,0,0,1,0],
+               [0,9,0,0,0,0,4,0,0]]
+
+    gridOG5 = [[0,2,8,1,0,0,0,0,0],
+               [4,0,0,0,8,0,0,3,0],
+               [0,0,0,0,2,0,0,0,0],
+               [7,3,0,0,0,0,0,0,6],
+               [0,0,4,0,0,0,8,5,0],
+               [0,0,5,0,0,9,0,0,0],
+               [0,0,2,0,3,6,0,0,9],
+               [8,0,0,9,0,0,0,0,7],
+               [0,6,0,2,0,0,0,4,0]]
+
+    gridOG6 = [[7,9,0,0,0,0,0,5,0],
+               [4,0,0,6,0,0,0,0,0],
+               [0,0,0,7,4,2,0,6,0],
+               [0,7,9,5,0,0,0,4,8],
+               [0,0,0,0,0,0,0,0,0],
+               [8,3,0,0,0,4,9,1,0],
+               [0,4,0,1,2,9,0,0,0],
+               [0,0,0,0,0,6,0,0,7],
+               [0,6,0,0,0,0,0,9,3]]
+
+    fails =   [[7,9,0,0,0,0,0,5,0],
+               [4,0,0,6,0,0,0,0,0],
+               [0,9,0,7,4,2,0,6,0],
+               [0,7,9,5,0,0,0,4,8],
+               [0,0,0,0,0,0,0,0,0],
+               [8,3,0,0,0,4,9,1,0],
+               [0,4,0,1,2,9,0,0,0],
+               [0,0,0,0,0,6,0,0,7],
+               [0,6,0,0,0,0,0,9,3]]
+    testCases = [grid, gridOG, gridOG2, gridOG3, gridOG4, gridOG5, gridOG6, hardest, fails]
+    #run some tests to see if you broke anything
+    print('solving test sudoku grids...')
+    i = 0
+    for test in testCases :
+        i = i + 1
+        print('Sudoku Puzzle ' + str(i) + ' of ' + str(len(testCases)))
+        printGrid(test)
+        attempt = solver(test)
+        if attempt :
+            printGrid(attempt)
+        print('')
+    print('success!')
+
+#main function
+print('Select feature.')
+print('    (1) Enter suduko problem to be solved.')
+print('    (2) Run example cases.')
+print('    (3) Exit program.')
+print('Enter feature number : ', end='')
+selection = input()
+if selection == '1' :
+    attempt = solver(enterProblem())
+    if attempt :
+        printGrid(attempt)
+elif selection == '2' :
+    runTest()
+else :
+    print('exiting program')
